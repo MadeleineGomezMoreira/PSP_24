b7659ade05432c0a6b175f9a833260eebc3bb88e
Clara Madeleine Gómez Moreira <tosigether598@gmail.com>
2024-03-11 19:18:09 +0100

    started graphql project + pom and settings were set up

---
+++ graphql_server_teams/src/main/java/com/teams/graphql_server_teams/GraphqlServerTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.teams.graphql_server_teams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlServerTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlServerTeamsApplication.class, args);
+	}
+
+}
---
+++ graphql_server_teams/src/test/java/com/teams/graphql_server_teams/GraphqlServerTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teams.graphql_server_teams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlServerTeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
